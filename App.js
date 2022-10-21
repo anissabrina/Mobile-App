0e382c7df54e636e1a3b3576a414a9cd01f239a8
sarahhnnn <sarah.manah@live.iium.edu.my>
2022-10-21 23:37:47 +0800

    test_v1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontsize:20 }}> Test :3 </Text>
+      <Text>Another version by Sarah!</Text>
       <StatusBar style="auto" />
     </View>
   );
