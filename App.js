4d253ca0712e55b41dab327e128f4d21c469de21
Sarah H <81020859+sarahhnnn@users.noreply.github.com>
2022-10-21 23:51:08 +0800

    test_v1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontsize:20 }}> Test :3 </Text>
+      <Text>Another version by Sarah!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff0f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
