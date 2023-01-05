1612d1192ebbe644b0938fecddaadcfaf7310747
Afif Azmin <116141263+aiazmin@users.noreply.github.com>
2023-01-05 21:08:05 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const LoginForm = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [matricNo, setMatricNo] = useState('');
+
+  const handleLogin = () => {
+    // You can add your own logic here to validate the form and log the user in
+    console.log(`Logging in with name: ${name} and matric no: ${matricNo}`);
+    navigation.navigate('Home');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={{ fontsize:20 }}> Test :3 </Text>
-      <Text>Another version by Sarah!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Welcome!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your matric no"
+        value={matricNo}
+        onChangeText={setMatricNo}
+      />
+      <Button title="Log In" onPress={handleLogin} />
     </View>
   );
-}
+};
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the home page!</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginForm} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff0f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
 });
+
+export default App;
