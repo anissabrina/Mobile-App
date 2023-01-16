bb5ddc4e2b9143747bd5a185e525b943e4b1f846
Sarah H <81020859+sarahhnnn@users.noreply.github.com>
2023-01-16 12:38:24 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import SignIn from "./screens/SignIn.js";
+import Home from "./screens/signedIn/Home";
+import Booking from "./screens/signedIn/Booking";
+import Setting from "./screens/signedIn/Setting";
+import VenueDetails from "./screens/signedIn/VenueDetails";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontsize:20 }}> Test :3 </Text>
-      <Text>Another version by Sarah!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: () => Node = ({ navigation }) => {
+  const [isSignedIn, setIsSignedIn] = useState(true);
+  const Stack = createNativeStackNavigator();
+  const HomeStack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff0f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const HomeStackScreens = ({ navigation }) => {
+    return (
+      <HomeStack.Navigator>
+        <HomeStack.Screen
+          name="Feed"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={focused ? "#000" : "grey"}
+                size={focused ? 30 : 20}
+              />
+            ),
+          }}
+        />
+        <HomeStack.Screen
+          name="VenueDetails"
+          component={VenueDetails}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={focused ? "#000" : "grey"}
+                size={focused ? 30 : 20}
+              />
+            ),
+          }}
+        />
+      </HomeStack.Navigator>
+    );
+  };
+
+  if (isSignedIn == true) {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Venues"
+            component={HomeStackScreens}
+            options={{
+              tabBarLabel: "Home",
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons
+                  name="home"
+                  color={focused ? "#000" : "grey"}
+                  size={focused ? 30 : 20}
+                />
+              ),
+            }}
+          />
+          {/* <Tab.Screen
+            name="Booking"
+            component={Booking}
+            options={{
+              tabBarLabel: "Booking",
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons
+                  name="archive"
+                  color={focused ? "#000" : "grey"}
+                  size={focused ? 30 : 20}
+                />
+              ),
+            }}
+          /> */}
+          <Tab.Screen
+            name="Sitting"
+            component={Setting}
+            options={{
+              tabBarLabel: "Setting",
+              headerShown: false,
+              tabBarIcon: ({ color, focused }) => (
+                <MaterialCommunityIcons
+                  name="cogs"
+                  color={focused ? "#000" : "grey"}
+                  size={focused ? 30 : 20}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+};
+
+export default App;
