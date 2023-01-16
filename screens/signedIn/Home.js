b4f759e4811dcff2adc6ab7a0046d06b37d4e8f8
Sarah H <81020859+sarahhnnn@users.noreply.github.com>
2023-01-16 13:13:49 +0800

    Update Home.js

--- screens/signedIn/Home.js
+++ screens/signedIn/Home.js
@@ -1,65 +1,74 @@
 import React, { useState, useEffect } from "react";
-import {
-  Text,
-  StyleSheet,
-  View,
-  SafeAreaView,
-  FlatList,
-  TouchableOpacity,
-  TextInput,
-  Image,
-  ScrollView,
-  StatusBar,
-  Animated,
-  Dimensions,
-  ImageBackground,
-} from "react-native";
+import { Text, StyleSheet, View, FlatList, TouchableOpacity, TextInput, Image, ScrollView, StatusBar, } from "react-native";
 import Icon from "react-native-vector-icons/SimpleLineIcons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-// function Item({ title, capacity }) {
-//   return (
-//     <View>
-//       <Text>{title}</Text>
-//       <Text>{capacity}</Text>
-//     </View>
-//   );
-// }
+const Venue = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardView}>
+        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
+          <View style={styles.cardViewTop}>
+            <View style={styles.cardTitle}>
+              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
+                Conference Room
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.cardViewBottom}>
+            <Image
+              source={require("../../assets/venues/Conference-Room.png")}
+              style={styles.image}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
+          <View style={styles.cardViewTop}>
+            <View style={styles.cardTitle}>
+              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
+                Lecture Theatre
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.cardViewBottom}>
+            <Image
+              source={require("../../assets/venues/Lecture-Theatre.png")}
+              style={styles.image}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
+          <View style={styles.cardViewTop}>
+            <View style={styles.cardTitle}>
+              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
+                Main Auditorium
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.cardViewBottom}>
+            <Image
+              source={require("../../assets/venues/Main-Auditorium.png")}
+              style={styles.image}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const Home = ({ navigation }) => {
   const [searchInput, setSearchInput] = useState("");
   const [feed, setFeed] = useState([]);
 
-  const filterTitle = (title) =>
-    response.Venue.filter((result) => result.title === title);
-
-  //get feed
-  // useEffect(() => {
-  //   fetch(
-  //     "https://raw.githubusercontent.com/anissabrina/Mobile-App/Sarah/response.json"
-  //   )
-  //     .then((re) => re.json())
-  //     .then(() => {
-  //       setFeed(re.response);
-  //     });
-  // }, []);
-
   return (
     <View style={styles.main}>
       <View>
         <Text style={styles.heading}>Venues</Text>
-        {/* <Text
-          style={{
-            display: "flex",
-            marginTop: 5,
-            marginBottom: 15,
-            marginLeft: 25,
-          }}
-        >
-          Book now!
-        </Text> */}
         {/* <View style={styles.searchBar}>
           <TextInput
             value={searchInput}
@@ -79,72 +88,6 @@ const Home = ({ navigation }) => {
 };
 
 export default Home;
-
-const Venue = ({ title, response }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.cardView}>
-        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
-          <View style={styles.cardViewTop}>
-            <View style={styles.cardTitle}>
-              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
-                Conference Room
-              </Text>
-              {/* <TouchableOpacity onPress={() => {}}>
-              <Icon name="arrow-right" color="#fff" style={{ padding: 5 }} />
-            </TouchableOpacity> */}
-            </View>
-          </View>
-
-          <View style={styles.cardViewBottom}>
-            <Image
-              source={require("../../assets/venues/Conference-Room.png")}
-              style={styles.image}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
-          <View style={styles.cardViewTop}>
-            <View style={styles.cardTitle}>
-              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
-                Lecture Theatre
-              </Text>
-              {/* <TouchableOpacity onPress={() => {}}>
-              <Icon name="arrow-right" color="#fff" style={{ padding: 5 }} />
-            </TouchableOpacity> */}
-            </View>
-          </View>
-
-          <View style={styles.cardViewBottom}>
-            <Image
-              source={require("../../assets/venues/Lecture-Theatre.png")}
-              style={styles.image}
-            />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("VenueDetails")}>
-          <View style={styles.cardViewTop}>
-            <View style={styles.cardTitle}>
-              <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
-                Main Auditorium
-              </Text>
-              {/* <TouchableOpacity onPress={() => {}}>
-              <Icon name="arrow-right" color="#fff" style={{ padding: 5 }} />
-            </TouchableOpacity> */}
-            </View>
-          </View>
-
-          <View style={styles.cardViewBottom}>
-            <Image
-              source={require("../../assets/venues/Main-Auditorium.png")}
-              style={styles.image}
-            />
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   main: {
