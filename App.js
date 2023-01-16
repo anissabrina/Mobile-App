ca5666fae1d126229c10541163e2ccf3e378f546
anissabrina <73023997+anissabrina@users.noreply.github.com>
2023-01-16 11:36:54 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View,Text} from 'react-native';
+import {SelectList }from 'react-native-dropdown-select-list'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontsize:20 }}> Test :3 </Text>
-      <Text>Another version by Sarah!</Text>
-      <Text style={{ color: '#1e90ff', fontSize: 25}}>
-        Anis is adding new line </Text>
-      <StatusBar style="auto" />
+const App = () => {
+
+  const [selected, setSelected] = React.useState("");
+  const [categories, setCategories] = React.useState([]);
+  
+  const data = [
+    {key:'KOE Colloseum', value:'KOE Colloseum'},
+    {key:'KOE Main Audi A', value:'KOE Main Audi A'},
+    {key:'KOE Main Audi B', value:'KOE Main Audi B'},
+    {key:'Wadi Budi', value:'Wadi Budi'},
+  ]
+
+
+  return(
+    <View style={{paddingHorizontal:15,marginTop:15}}>
+
+      <Text style={{color:'blue'}}> Venue : </Text>
+      <SelectList 
+        setSelected={(val) => setCategories(val)} 
+        data={data} 
+        save="value"
+        label="Categories"
+        boxStyles={{marginTop:25}}
+    />
+
+
+
+      <View style={{marginTop:50}}>
+        <Text>Selected Value : </Text>
+        <Text style={{marginTop:10,color:'gray'}}>{selected}</Text>
+      </View>
     </View>
-  );
-}
+    
+  )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff0f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+
+export default App;
